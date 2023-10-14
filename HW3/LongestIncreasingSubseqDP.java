99afb88ea0ae77892fecceca09f2d6b3c07f2be0
md2383 <md2383@rit.edu>
2023-10-13 21:59:19 -0400

    HW3 almost done just #4

--- HW3/LongestIncreasingSubseqDP.java
+++ HW3/LongestIncreasingSubseqDP.java
@@ -20,8 +20,36 @@ public class LongestIncreasingSubseqDP {
         return max;
     }
 
+    public void generateInputs(int size) {
+
+        int[] A = new int[size];
+        int[] S = new int[size];
+
+        // generate random numbers into A
+        for (int i = 0; i < size; i++) {
+            A[i] = A[i] = (int) (Math.random() * 1000);
+            S[i] = 0;
+        }
+
+        long time = System.currentTimeMillis();
+
+        incrSubseqDP(S, A, size, 0);
+
+        long time2 = System.currentTimeMillis();
+
+        System.out.println("Size: " + size);
+        System.out.println("Time Difference: " + (time2 - time) + " ms");
+    }
+
     public static void main(String[] args) {
         LongestIncreasingSubseqDP lis = new LongestIncreasingSubseqDP();
+
+        // int count = 1;
+        // // Generate for 10 values
+        // for (int i = 10; i < 110; i += 10) {
+        // System.out.println(count++ + ".");
+        // lis.generateInputs(i);
+        // }
 
         java.util.Scanner sc = new java.util.Scanner(System.in);
 
@@ -39,5 +67,6 @@ public class LongestIncreasingSubseqDP {
         }
         int max = 0;
         System.out.println(lis.incrSubseqDP(S, A, size, max));
+
     }
 }
--- HW3/LongestIncreasingSubseqRecursive.java
+++ HW3/LongestIncreasingSubseqRecursive.java
@@ -1,25 +1,50 @@
 package HW3;
-
 import java.util.Scanner;
 
 public class LongestIncreasingSubseqRecursive {
 
-    public int incrSubseqRecursive(int[] A, int j) {
-        if (j == 0) {
-            return 1;
+    public int incrSubseqRecursive(int[] A, int j, int size, int max) {
+        if (j < size) {
+            return 0;
         }
-        int max = 0;
-        for (int i = 0; i < j; i++) {
-            int temp = incrSubseqRecursive(A, i);
-            if (A[i] < A[j] && temp > max) {
-                max = temp;
-            }
+        int withoutCurr = incrSubseqRecursive(A, j - 1, size, max);
+        int withCurr = 0;
+        if (A[j] < max) {
+            withCurr = incrSubseqRecursive(A, j - 1, size, A[j]) + 1;
         }
-        return 0;
+        return Math.max(withoutCurr, withCurr);
+    }
+
+    public void generateInputs(int size) {
+
+        int[] A = new int[size];
+
+        // generate random numbers into A
+        for (int i = 0; i < size; i++) {
+            A[i] = (int) (Math.random() * 1000);
+        }
+
+        long time = System.currentTimeMillis();
+
+        incrSubseqRecursive(A, size - 1, 0, Integer.MAX_VALUE);
+
+        long time2 = System.currentTimeMillis();
+
+        System.out.println("\n");
+        System.out.println("Size: " + size);
+        System.out.println("Time Difference: " + (time2 - time) + " ms");
+        System.out.println("\n");
     }
 
     public static void main(String[] args) {
         LongestIncreasingSubseqRecursive lis = new LongestIncreasingSubseqRecursive();
+
+        // int count = 1;
+        // // Generate for 10 values
+        // for (int i = 10; i < 110; i += 10) {
+        // System.out.println(count++ + ".");
+        // lis.generateInputs(i);
+        // }
 
         Scanner sc = new Scanner(System.in);
 
@@ -31,6 +56,8 @@ public class LongestIncreasingSubseqRecursive {
             A[i] = sc.nextInt();
         }
 
-        System.out.println(lis.incrSubseqRecursive(A, A.length - 1));
+        System.out.println(lis.incrSubseqRecursive(A, size - 1, 0,
+                Integer.MAX_VALUE));
+
     }
 }
